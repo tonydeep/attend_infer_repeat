* fix transition in DiscoveryCell: allowance is based on input * simplify what computation in PropCell

attend_infer_repeat/apdr.py
@@ -63,7 +63,9 @@ class AttendDiscoverRepeat(AIRBase):
         self._discover_only_t0 = discover_only_t0
 
     def _build(self, img, n_present_obj, conditioning=None, time_step=0):
-        hidden_outputs, num_steps = self._discover(img, n_present_obj, conditioning, time_step)
+        max_disc_steps = self._n_steps - n_present_obj
+
+        hidden_outputs, num_steps = self._discover(img, max_disc_steps, conditioning, time_step)
         kl, kl_where, kl_what, kl_num_step, num_steps_prob, log_num_steps_prob\
             = self._estimate_kl(hidden_outputs, num_steps, time_step)
 
@@ -75,14 +77,15 @@ class AttendDiscoverRepeat(AIRBase):
             kl=kl,
             kl_where=kl_where,
             kl_what=kl_what,
-            kl_num_step=kl_num_step
+            kl_num_step=kl_num_step,
+            max_disc_steps=max_disc_steps
         )
         outputs.update(BaseAPDRCell.outputs_by_name(hidden_outputs))
 
         return outputs
 
-    def _discover(self, img, n_present_obj, conditioning, time_step):
-        max_disc_steps = self._n_steps - n_present_obj
+    def _discover(self, img, max_disc_steps, conditioning, time_step):
+
         if self._discover_only_t0:
             first = tf.cast(tf.equal(time_step, 0), max_disc_steps.dtype)
             max_disc_steps *= first
@@ -98,7 +101,7 @@ class AttendDiscoverRepeat(AIRBase):
         presence = BaseAPDRCell.extract_latents(hidden_outputs, key='pres')
         num_steps = tf.reduce_sum(presence[..., 0], -1)
 
-        return hidden_outputs, num_steps
+        return hidden_outputs, num_steps,
 
     def _estimate_kl(self, hidden_outputs, num_steps, time_step):
         what, what_loc, what_scale, where, where_loc, where_scale, presence_prob, presence, presence_logit \

attend_infer_repeat/cell.py
@@ -97,7 +97,7 @@ class BaseAPDRCell(snt.RNNCore):
 
     @staticmethod
     def outputs_by_name(hidden_outputs):
-        return {n: o for n, o in zip(DiscoveryCell._output_names, hidden_outputs)}
+        return {n: o for n, o in zip(BaseAPDRCell._output_names, hidden_outputs)}
 
     @staticmethod
     def extract_latents(hidden_outputs, key=None, skip=None):
@@ -215,7 +215,7 @@ class DiscoveryCell(BaseAPDRCell):
 
     def _parse_inpt(self, inpt, presence):
         inpt, is_allowed = inpt
-        return inpt, is_allowed * presence
+        return inpt, is_allowed
 
     def _compute_what(self, inpt, img, where_code):
         what_params = self._extract_and_encode_glimpse(img, where_code)
@@ -256,7 +256,6 @@ class PropagationCell(BaseAPDRCell):
                                               decoder=decoder, debug=debug)
 
         with self._enter_variable_scope():
-            self._what_transform = MLP([self._n_hidden] * 2)
             self._latent_scale = latent_scale
 
     def _parse_inpt(self, inpt, _):
@@ -266,10 +265,8 @@ class PropagationCell(BaseAPDRCell):
 
     def _compute_what(self, inpt, img, where_code):
         what_tm1 = inpt[0]
-        code = self._extract_and_encode_glimpse(img, where_code)
-
-        inpt = tf.concat((code, what_tm1), -1)
-        what_params = self._what_transform(inpt)
+        what_params = self._extract_and_encode_glimpse(img, where_code)
+        # what_params = tf.concat((what_params, what_tm1), -1)
         what_distrib = self._what_distrib(what_params)
         return what_distrib.sample(), what_distrib.loc, what_distrib.scale
 

test/apdr_test.py
@@ -74,7 +74,7 @@ class DiscoverTest(ModuleTest, unittest.TestCase):
 
     def test_shapes(self):
         num_hidden_outputs = 9
-        num_other_outputs = 8
+        num_other_outputs = 9
 
         self.assertEqual(len(self.output.hidden_outputs), num_hidden_outputs)
         self.assertEqual(len(self.output), num_other_outputs + len(self.output.hidden_outputs))
@@ -93,7 +93,20 @@ class DiscoverTest(ModuleTest, unittest.TestCase):
         values, num_present_objects = self.sess.run([self.output, self.num_present_objects], {self.img: random_img})
         max_allowed_objects = self.n_steps - num_present_objects
 
+        # num steps should be <= max_steps - num_present_objects
+        assert_array_equal(values.max_disc_steps, max_allowed_objects)
         assert_array_less(values.num_steps, max_allowed_objects + 1)
+
+        # check for allowed values
+        assert_array_equal(values.num_steps, np.round(values.num_steps))
+        assert_array_less(values.num_steps_prob, 1.01)
+        assert_array_less(-.01, values.num_steps_prob)
+
+        for i, allowed_n_steps in enumerate(values.max_disc_steps):
+            idx = int(min(allowed_n_steps, self.n_steps))
+            self.assertNotEqual(values.num_steps_prob[i, idx], 0.)
+            if idx < self.n_steps:
+                self.assertEqual(values.num_steps_prob[i, idx + 1], 0.)
 
         for k, v in values.iteritems():
             self.assertFalse(flatten_check(v, np.isnan), 'NaNs in {}'.format(k))
@@ -105,7 +118,7 @@ class DiscoverTest(ModuleTest, unittest.TestCase):
         self.assertGreaterEqual(values.kl_where.mean(), 0.)
         self.assertGreaterEqual(values.kl_num_step.mean(), 0.)
 
-        #       KL what & where should be equal to zero for objects that are not there
+        # KL what & where should be equal to zero for objects that are not there
         presence = values.presence[..., 0]
         assert_array_equal(values.kl_what, values.kl_what * presence)
         assert_array_equal(values.kl_where, values.kl_where * presence)
@@ -160,6 +173,11 @@ class PropagateTest(ModuleTest, unittest.TestCase):
         random_img = np.random.uniform(size=self.img.shape.as_list())
         values, pres_tm1 = self.sess.run([self.output, self.pres_tm1], {self.img: random_img})
         pres_tm1 = pres_tm1[..., 0]
+
+        # check for allowed values
+        assert_array_equal(values.num_steps, np.round(values.num_steps))
+        assert_array_less(values.prop_prob, 1.01)
+        assert_array_less(-.01, values.prop_prob)
 
         for k, v in values.iteritems():
             self.assertFalse(flatten_check(v, np.isnan), 'NaNs in {}'.format(k))
@@ -372,6 +390,10 @@ class APDRTest(unittest.TestCase):
         self.assertGreaterEqual(values.kl_what.mean(), 0.)
         self.assertGreaterEqual(values.kl_where.mean(), 0.)
         self.assertGreaterEqual(values.kl_num_step.mean(), 0.)
+
+        # check for allowed values
+        assert_array_equal(values.num_steps, np.round(values.num_steps))
+        assert_array_equal(values.num_steps, values.prop.num_steps + values.disc.num_steps)
 
     def test_all_zero_presence(self):
         """No objects should be propagated; all ids should be introduced by discovery and different than previous ids"""